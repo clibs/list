2fa738da0b578506b93389deb8c82ad9d5c55d5a
Tj Holowaychuk <tj@vision-media.ca>
2010-11-27 21:34:13 -0800

    newlines

--- src/list.h
+++ src/list.h
@@ -70,26 +70,51 @@ typedef struct {
 
 // Node prototypes.
 
-list_node_t *list_node_new(void *val);
+list_node_t *
+list_node_new(void *val);
 
 // list_t prototypes.
 
-list_t *list_new();
-list_node_t *list_push(list_t *self, list_node_t *node);
-list_node_t *list_unshift(list_t *self, list_node_t *node);
-list_node_t *list_find(list_t *self, void *val);
-list_node_t *list_at(list_t *self, int index);
-list_node_t *list_pop(list_t *self);
-list_node_t *list_shift(list_t *self);
-void list_remove(list_t *self, list_node_t *node);
-void list_destroy(list_t *self);
+list_t *
+list_new();
+
+list_node_t *
+list_push(list_t *self, list_node_t *node);
+
+list_node_t *
+list_unshift(list_t *self, list_node_t *node);
+
+list_node_t *
+list_find(list_t *self, void *val);
+
+list_node_t *
+list_at(list_t *self, int index);
+
+list_node_t *
+list_pop(list_t *self);
+
+list_node_t *
+list_shift(list_t *self);
+
+void
+list_remove(list_t *self, list_node_t *node);
+
+void
+list_destroy(list_t *self);
 
 // list_t iterator prototypes.
 
-list_iterator_t *list_iterator_new(list_t *list, list_direction_t direction);
-list_iterator_t *list_iterator_new_from_node(list_node_t *node, list_direction_t direction);
-list_node_t *list_iterator_next(list_iterator_t *self);
-void list_iterator_destroy(list_iterator_t *self);
+list_iterator_t *
+list_iterator_new(list_t *list, list_direction_t direction);
+
+list_iterator_t *
+list_iterator_new_from_node(list_node_t *node, list_direction_t direction);
+
+list_node_t *
+list_iterator_next(list_iterator_t *self);
+
+void
+list_iterator_destroy(list_iterator_t *self);
 
 #ifdef __cplusplus
 }
